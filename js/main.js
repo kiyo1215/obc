73438e08b75155c6c806b697745154cff7f69456
kiyo1215 <g.k.n15@icloud.com>
2022-01-08 03:44:08 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// function keisan() {
+
+//   let price1 = document.form.castDrinkQuantity.selectedIndex * 1000;
+//   document.from.field1.value = price1;
+  
+// }
+
+function calc(castDrinkQuantity) {
+  let price1 = document.getElementById('field1').value =
+    Math.round(castDrinkQuantity * 1000);
+}
