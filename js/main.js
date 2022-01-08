bc9476ee4ee8ef8cea5f552b6d29a67f6c234957
kiyo1215 <g.k.n15@icloud.com>
2022-01-08 22:05:30 +0900

    update2205

--- js/main.js
+++ js/main.js
@@ -16,20 +16,35 @@ function calc(
   champagneC,
   champagneD,
   champagneE,
+  champagneF,
+  champagneG,
+  champagneH,
+  champagneI,
 ){
   const price1 = document.getElementById('field1').value =
-    Math.round(castDrinkQuantity * 1000);
+    Math.round(castDrinkQuantity * 1200);
   
   const price2 = document.getElementById('feild2').value =
-    Math.round(cheki_quantity * 1000);
+    Math.round(cheki_quantity * 1400);
   
   const price3 = document.getElementById('feild3').value =
     Math.round(food_quantity * 1000);
   
   const time1 = Math.round((go_hour - come_hour) * 60);
   const time2 = Math.round(go_minute - come_minute);
-  const time3 = document.getElementById('extension_q').value =
-    Math.ceil((time1 + time2 - first) / 20);
+  
+  if (first === '60'){
+      const time3 = document.getElementById('extension_q').value =
+      Math.ceil((time1 + time2 - 2500) / 20);
+  } else if (first === '90') {
+    const time3 = document.getElementById('extension_q').value =
+      Math.ceil((time1 + time2 - 3500) / 20);
+  } else if (first === '60-2'){
+    const time3 = document.getElementById('extension_q').value =
+       Math.ceil((time1 + time2 - 3000) / 20);
+    };
+  
+  
   const price4 = document.getElementById('extension_p').value =
     Math.round(time3 * 1000);
   const price5 = document.getElementById('outher_p').value =
@@ -38,22 +53,31 @@ function calc(
     Math.round(outher_m * 1);
   
   const price7 = document.getElementById('feild4').value = 
-    Math.round(champagneA * 5000);
+    Math.round(champagneA * 7000);
   const price8 = document.getElementById('feild5').value = 
-    Math.round(champagneB * 10000);
+    Math.round(champagneB * 70000);
   const price9 = document.getElementById('feild6').value = 
-    Math.round(champagneC * 15000);
+    Math.round(champagneC * 10000);
   const price10 = document.getElementById('feild7').value = 
-    Math.round(champagneD * 20000);
+    Math.round(champagneD * 23000);
   const price11 = document.getElementById('feild8').value = 
-    Math.round(champagneE * 25000);
+    Math.round(champagneE * 29000);
+  const price12 = document.getElementById('feild9').value = 
+    Math.round(champagneF * 35000);
+  const price13 = document.getElementById('feild10').value = 
+    Math.round(champagneG * 57000);
+  const price14 = document.getElementById('feild11').value = 
+    Math.round(champagneH * 92000);
+  const price15 = document.getElementById('feild12').value = 
+    Math.round(champagneI * 138000);
   
     if (first === '60') {
       const time4 = document.getElementById('all_p').value =
-      Math.round(price1 + price2 + price3 + price4 + price5 - price6 + price7 + price8 + price9 + price10 + price11 + 1000);
+        Math.round(price1 + price2 + price3 + price4 + price5 - price6 + price7 + price8 + price9 + price10
+          + price11 + price12 + price13 + price14 + price15 + 3000);
   } else if(first === '90') {
     const time4 = document.getElementById('all_p').value =
-    Math.round(price1 + price2 + price3 + price4 + price5 - price6 + price7 + price8 + price9 + price10 + price11 + 2000);
+      Math.round(price1 + price2 + price3 + price4 + price5 - price6 + price7 + price8 + price9 + price10 + price11 + price12 + price13 + price14 + price15 + 3500);
   };
     
 };
